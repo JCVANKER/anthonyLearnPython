19f42b7619539f44cc7bd323aa445be5be87a816
Anthony <825958813@qq.com>
2018-03-20 14:58:15 +0800

    2018/3/20

--- learning_Python/basis/用户输入和while循环/函数input()、int()、求模/input_int_%.py
+++ learning_Python/basis/用户输入和while循环/函数input()、int()、求模/input_int_%.py
@@ -1,5 +1,5 @@
 #函数input()接受一个参数：即要向用户显示的提示或说明；返回用户输入的字符串
-message=input("Tell me somethin, and I will repeat it bank to you:")
+message=input("Tell me something, and I will repeat it bank to you:")
 print("Hello "+message+"!")
 print("--------------------------------------------------------------")
 prompt="If you tell us who you are,we can personalize the message a"
